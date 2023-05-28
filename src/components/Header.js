c2a6510f4523fae3b0a6ee847c1406ea1c328755
amirka_linux <far20022002@mail.ru>
2023-05-28 18:28:55 +0500

    added empty header, item_adder and changed App component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+function Header(props) {
+    return (
+        <div className={'header'}>
+            <div className={'text'}>TO DO</div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Item_adder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Item_adder.css'
+function Item_adder(props) {
+    return (
+        <div className={'wrapper'}>
+            <input />
+        </div>
+    );
+}
+
+export default Item_adder;
